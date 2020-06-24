baf2f0d88bd73ad17fff7a34db2d5df1d2d9b0b5
manekmehra <61108498+manekmehra@users.noreply.github.com>
2020-06-24 15:26:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+
+var r,g,b
+
+
+
+function setup(){
+
+  createCanvas(1200,400);
+  
+}
+
+
+function draw(){
+
+ if (keyDown("R")){
+   background(255,0,0)
+ }
+ if (keyDown("G")){
+  background(65,117,78)
+}
+if (keyDown("B")){
+  background(0,0,255)
+}
+}
